6ee214b50d031f4e6504d49e8619bc8bf2356713
loading <49134864+load1n9@users.noreply.github.com>
2022-09-30 22:57:13 -0400

    feat: fix iterate2d matrices

--- src/cpu/matrix.ts
+++ src/cpu/matrix.ts
@@ -1,5 +1,5 @@
-import { DataTypeArray, DataType } from "../../deps.ts";
-import { iterate1D } from "../util.ts";
+import { DataType, DataTypeArray } from "../../deps.ts";
+import { iterate1D, iterate2D } from "../util.ts";
 export class CPUMatrix<T extends DataType = DataType> {
   deltas: DataTypeArray<T>;
   constructor(
@@ -20,80 +20,78 @@ export class CPUMatrix<T extends DataType = DataType> {
 
   static add(matA: CPUMatrix, matB: CPUMatrix) {
     const res = CPUMatrix.with(matA.x, matA.y);
-    for (let i = 0; i < matA.data.length; i++) {
+    iterate1D(matA.data.length, (i: number) => {
       res.data[i] = matA.data[i] + matB.data[i];
-    }
+    });
     return res;
   }
 
   static dot(matA: CPUMatrix, matB: CPUMatrix) {
     const res = CPUMatrix.with(matB.x, matA.y);
-    for (let x = 0; x < matB.x; x++) {
-      for (let y = 0; y < matA.y; y++) {
-        let sum = 0;
-        for (let k = 0; k < matA.x; k++) {
-          const a = k + y * matA.x;
-          const b = x + k * matB.x;
-           sum += matA.data[a] * matB.data[b];
-        }
-        const idx = x + y * matB.x;
-        res.data[idx] = sum;
-      }
-    }
+    iterate2D({ x: matB.x, y: matA.y }, (x: number, y: number) => {
+      let sum = 0;
+      iterate1D(matA.x, (k: number) => {
+        const a = k + y * matA.x;
+        const b = x + k * matB.x;
+        sum += matA.data[a] * matB.data[b];
+      });
+      const idx = x + y * matB.x;
+      res.data[idx] = sum;
+    });
     return res;
   }
 
   static Tdot(matA: CPUMatrix, matB: CPUMatrix) {
     const res = CPUMatrix.with(matB.y, matA.y);
-    for (let x = 0; x < matB.y; x++) {
-      for (let y = 0; y < matA.y; y++) {
-        let sum = 0;
-        for (let k = 0; k < matA.x;  k++) {
-          const a = k + y * matA.x;
-          const b = k + x * matB.x;
-          sum += matA.data[a] * matB.data[b];
-        }
-        const idx = x + y * matB.y;
-        res.data[idx] = sum;
-      }
-    }
+    iterate2D({ x: matB.y, y: matA.y }, (x: number, y: number) => {
+      let sum = 0;
+      iterate1D(matA.x, (k: number) => {
+        const a = k + y * matA.x;
+        const b = k + x * matB.x;
+        sum += matA.data[a] * matB.data[b];
+      });
+      const idx = x + y * matB.y;
+      res.data[idx] = sum;
+    });
     return res;
   }
 
   static sub(matA: CPUMatrix, matB: CPUMatrix) {
     const res = CPUMatrix.with(matA.x, matA.y);
-    for (let i = 0; i < matA.data.length; i++) {
+    iterate1D(matA.data.length, (i: number) => {
       res.data[i] = matA.data[i] - matB.data[i];
-    }
-    return res;
-  }
-  static reduce(mat: CPUMatrix, func: (acc: number, val: number) => number) {
-    for (let i = 0; i < mat.data.length; i++) {
-      mat.data[0] = func(mat.data[0], mat.data[i]);
-    }
-    return mat;
-  }
-  static mirror(mat: CPUMatrix) {
-    for (let i = 0; i < mat.data.length; i++) {
-      mat.data[i] = mat.data[mat.data.length - i - 1];
-    }
-    return mat;
-  }
-  static transpose(mat: CPUMatrix) {
-    const res = CPUMatrix.with(mat.y, mat.x);
-    for (let x = 0; x < mat.x; x++) {
-      for (let y = 0; y < mat.y; y++) {
-        const i = x + y * mat.x;
-        const j = y + x * mat.y;
-        res.data[j] = mat.data[i];
-      }
-    }
+    });
     return res;
   }
 
+  static reduce(mat: CPUMatrix, func: (acc: number, val: number) => number) {
+    iterate1D(mat.data.length, (i: number) => {
+      mat.data[0] = func(mat.data[0], mat.data[i]);
+    });
+    return mat;
+  }
+
+  static mirror(mat: CPUMatrix) {
+    iterate1D(mat.data.length, (i: number) => {
+      mat.data[i] = mat.data[mat.data.length - i - 1];
+    });
+    return mat;
+  }
+
+  static transpose(mat: CPUMatrix) {
+    const res = CPUMatrix.with(mat.y, mat.x);
+    iterate2D(mat, (x: number, y: number) => {
+      const i = x + y * mat.x;
+      const j = y + x * mat.y;
+      res.data[j] = mat.data[i];
+    });
+    return res;
+  }
+  
   fill(val: number) {
     this.data.fill(val);
   }
+
   getData(x: number, y: number) {
     const ix = this.y * x + y;
     if (ix < 0 || ix >= this.data.length) {
@@ -101,6 +99,7 @@ export class CPUMatrix<T extends DataType = DataType> {
     }
     return this.data[ix];
   }
+
   setData(x: number, y: number, val: number) {
     const ix = this.y * x + y;
     if (ix < 0 || ix >= this.data.length) {
@@ -108,6 +107,7 @@ export class CPUMatrix<T extends DataType = DataType> {
     }
     this.data[ix] = val;
   }
+
   getDelta(x: number, y: number) {
     const ix = this.y * x + y;
     if (ix < 0 || ix >= this.deltas.length) {
@@ -115,6 +115,7 @@ export class CPUMatrix<T extends DataType = DataType> {
     }
     return this.deltas[ix];
   }
+
   setDelta(x: number, y: number, val: number) {
     const ix = this.y * x + y;
     if (ix < 0 || ix >= this.deltas.length) {
@@ -122,6 +123,7 @@ export class CPUMatrix<T extends DataType = DataType> {
     }
     this.deltas[ix] = val;
   }
+
   toJSON() {
     return {
       data: this.data,
@@ -132,12 +134,12 @@ export class CPUMatrix<T extends DataType = DataType> {
   fmt() {
     let res = "";
     iterate1D(this.y, (i: number) => {
-      const row = this.data.slice(i * this.x, (i+ 1) * this.x);
+      const row = this.data.slice(i * this.x, (i + 1) * this.x);
       iterate1D(row.length, (j: number) => {
-        res += row[j].toString() + " "
+        res += row[j].toString() + " ";
       });
       res += "\n";
-    })
+    });
     return res;
   }
 }
