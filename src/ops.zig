2a02c6ddfd87141ee301b9efa80f3f384150adb6
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-03-17 11:54:51 -0400

    feat: added more ops

--- src/ops.zig
+++ src/ops.zig
@@ -14,6 +14,103 @@ pub const op_tensor_free = *const fn (
     tensor: *tensor.Tensor,
 ) void;
 
+pub const op_tensor_sum = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+    dtype: tensor.TensorType,
+) *tensor.Tensor;
+
+pub const op_tensor_matmul = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+    tensor_other: *tensor.Tensor,
+    dtype: tensor.TensorType,
+) *tensor.Tensor;
+
+pub const op_tensor_transpose = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+    dim0: u8,
+    dim1: u8,
+) *tensor.Tensor;
+
+pub const op_tensor_sigmoid = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_softmax = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+    dim: u8,
+    dtype: tensor.TensorType,
+) *tensor.Tensor;
+
+pub const op_tensor_abs = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_acos = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_asin = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_atan = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_cos = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_sin = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_tan = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_cosh = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_sinh = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_tanh = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_ceil = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_sqrt = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
+pub const op_tensor_lgamma = *const fn (
+    ctx: *const Context,
+    tensor: *tensor.Tensor,
+) *tensor.Tensor;
+
 pub const op_tensor_reshape = *const fn (
     ctx: *const Context,
     tensor: *tensor.Tensor,
@@ -43,89 +140,94 @@ pub const op_tensor_inner = *const fn (
 
 pub const op_tensor_foreach_abs = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_acos = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_asin = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_atan = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_cos = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_sin = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_tan = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_cosh = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_sinh = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_tanh = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_ceil = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_sqrt = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const op_tensor_foreach_lgamma = *const fn (
     ctx: *const Context,
-    tensor: []*tensor.Tensor,
-    other_tensor: *tensor.Tensor,
+    tensors: []*tensor.Tensor,
 ) []*tensor.Tensor;
 
 pub const Ops = struct {
     tensor_new: op_tensor_new,
     tensor_free: op_tensor_free,
+    tensor_sum: op_tensor_sum,
+    tensor_matmul: op_tensor_matmul,
+    tensor_transpose: op_tensor_transpose,
+    tensor_sigmoid: op_tensor_sigmoid,
+    tensor_softmax: op_tensor_softmax,
     tensor_reshape: op_tensor_reshape,
     tensor_flatten: op_tensor_flatten,
     tensor_dot: op_tensor_dot,
     tensor_inner: op_tensor_inner,
+    tensor_abs: op_tensor_abs,
+    tensor_acos: op_tensor_acos,
+    tensor_asin: op_tensor_asin,
+    tensor_atan: op_tensor_atan,
+    tensor_ceil: op_tensor_ceil,
+    tensor_cos: op_tensor_cos,
+    tensor_sin: op_tensor_sin,
+    tensor_tan: op_tensor_tan,
+    tensor_cosh: op_tensor_cosh,
+    tensor_sinh: op_tensor_sinh,
+    tensor_tanh: op_tensor_tanh,
+    tensor_sqrt: op_tensor_sqrt,
+    tensor_lgamma: op_tensor_lgamma,
     tensor_foreach_abs: op_tensor_foreach_abs,
     tensor_foreach_acos: op_tensor_foreach_acos,
     tensor_foreach_asin: op_tensor_foreach_asin,
