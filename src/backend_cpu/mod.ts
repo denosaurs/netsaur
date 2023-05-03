cefcfe559f893728930e0d3dc65c4b4a92293419
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-05-03 10:12:25 -0400

    feat: documentation

--- src/backend_cpu/mod.ts
+++ src/backend_cpu/mod.ts
@@ -11,7 +11,7 @@ const options: FetchOptions = {
   name: "netsaur",
   url: new URL(import.meta.url).protocol !== "file:"
     ? new URL(
-      "https://github.com/denosaurs/netsaur/releases/download/0.2.0/",
+      "https://github.com/denosaurs/netsaur/releases/download/0.2.2/",
       import.meta.url,
     )
     : "./target/release/",
