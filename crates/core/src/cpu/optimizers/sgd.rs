78d0c997d0df2b7e86a1754711695418e41e3066
Pranev (NeTT) <nett@nett.moe>
2023-08-21 07:38:33 +0530

    feat: Learning Rate Schedulers (#37)
    * feat: learning rate schedulers
    
    * fix decay schedulers

--- crates/core/src/cpu/optimizers/sgd.rs
+++ crates/core/src/cpu/optimizers/sgd.rs
@@ -2,6 +2,8 @@ use std::ops::{SubAssign, Mul};
 
 use ndarray::{ArrayViewD, ArrayViewMutD};
 
+use crate::CPUScheduler;
+
 pub struct CPUSGDOptimizer {}
 
 impl CPUSGDOptimizer {
@@ -13,10 +15,13 @@ impl CPUSGDOptimizer {
         &mut self,
         mut params: Vec<ArrayViewMutD<f32>>,
         grads: Vec<ArrayViewD<f32>>,
+        scheduler: &CPUScheduler,
         rate: f32,
+        epoch: usize,
     ) {
+        let eta = scheduler.eta(rate, epoch);
         for (param, grad) in params.iter_mut().zip(grads) {
-            param.sub_assign(&grad.mul(rate))
+            param.sub_assign(&grad.mul(eta));
         }
     }
 }
