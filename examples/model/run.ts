ec7e52eafd453003ae8cdff27fd546fe61418c45
NeTT <nett@nett.moe>
2024-01-28 14:53:11 +0530

    update saved model example

--- examples/model/run.ts
+++ examples/model/run.ts
@@ -1,4 +1,4 @@
-import { CPU, Sequential, setupBackend, tensor1D } from "../../mod.ts";
+import { CPU, Sequential, setupBackend, tensor2D } from "../../mod.ts";
 
 /**
  * Setup the CPU backend. This backend is fast but doesn't work on the Edge.
@@ -10,14 +10,14 @@ const model = Sequential.loadFile("examples/model/xor.test.st");
 /**
  * Predict the output of the XOR function for the given inputs.
  */
-const out1 = (await model.predict(tensor1D([0, 0]))).data;
+const out1 = (await model.predict(tensor2D([[0, 0]]))).data;
 console.log(`0 xor 0 = ${out1[0]} (should be close to 0)`);
 
-const out2 = (await model.predict(tensor1D([1, 0]))).data;
+const out2 = (await model.predict(tensor2D([[1, 0]]))).data;
 console.log(`1 xor 0 = ${out2[0]} (should be close to 1)`);
 
-const out3 = (await model.predict(tensor1D([0, 1]))).data;
+const out3 = (await model.predict(tensor2D([[0, 1]]))).data;
 console.log(`0 xor 1 = ${out3[0]} (should be close to 1)`);
 
-const out4 = (await model.predict(tensor1D([1, 1]))).data;
+const out4 = (await model.predict(tensor2D([[1, 1]]))).data;
 console.log(`1 xor 1 = ${out4[0]} (should be close to 0)`);
