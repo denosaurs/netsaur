7024bff2fef1fd66a26050e0fe9ff7741133a00c
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-09-05 10:44:31 -0400

    fix: exports (#55)

--- web.ts
+++ web.ts
@@ -1,10 +1,10 @@
-export { setupBackend } from "./packages/core/core/engine.ts";
-export * from "./packages/core/core/mod.ts";
-export * from "./packages/core/core/types.ts";
-export * from "./packages/core/core/tensor/tensor.ts";
-export * from "./packages/core/core/api/layers.ts";
-export * from "./packages/core/core/api/shape.ts";
-export * from "./packages/core/core/api/network.ts";
-export * from "./packages/core/core/api/optimizer.ts";
+export { setupBackend } from "./packages/core/src/core/engine.ts";
+export * from "./packages/core/src/core/mod.ts";
+export * from "./packages/core/src/core/types.ts";
+export * from "./packages/core/src/core/tensor/tensor.ts";
+export * from "./packages/core/src/core/api/layers.ts";
+export * from "./packages/core/src/core/api/shape.ts";
+export * from "./packages/core/src/core/api/network.ts";
+export * from "./packages/core/src/core/api/optimizer.ts";
 
-export { WASM } from "./packages/core/backends/wasm/mod.ts";
+export { WASM } from "./packages/core/src/backends/wasm/mod.ts";
