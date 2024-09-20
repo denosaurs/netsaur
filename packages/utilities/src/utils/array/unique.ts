1d5a750260b7be076228eccb88bb601ad8f9f410
Pranev (NeTT) <nett@nett.moe>
2024-09-20 22:46:47 +0530

    feat: Time per epoch and ETA logging when `silent=false` (#64)
    * add time logger
    
    * return vec on fit
    
    * use Set
    
    * use logistic reg

--- packages/utilities/src/utils/array/unique.ts
+++ packages/utilities/src/utils/array/unique.ts
@@ -6,5 +6,6 @@
  */
 export function useUnique<T>(arr: ArrayLike<T>): T[] {
   const array = Array.from(arr);
-  return array.filter((x, i) => array.indexOf(x) === i);
+  return [...new Set(array)]
+//  return array.filter((x, i) => array.indexOf(x) === i);
 }
